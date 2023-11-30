f03d5f61da1639eebb6e7ccea70b513f631ad49d
kyledomen <kyle.domen@sjsu.edu>
2023-11-29 21:26:28 -0800

    execStatement() working with .java .cpp & .py files

--- Api.java
+++ Api.java
@@ -1,11 +1,15 @@
 import java.io.*;
 import java.nio.file.*;
+import javax.tools.*;
 import java.awt.Desktop;
 import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.*;
 
-public class Api {
+public class API {
 
-    public Api() {}
+    public API() {}
 
     public static void runListStatement(String directoryPath) {
         File directory = new File(directoryPath);
@@ -173,7 +177,140 @@ public class Api {
             System.out.println("Error: unable to replace Strings");
         }
     }
-    
+
+    public static void execStatement(String filePath) {
+        String fileType = getFileType(filePath);
+
+        try {
+            switch (fileType) {
+                case "java":
+                    execJava(filePath);
+                    break;
+                case "cpp":
+                    execCpp(filePath);
+                    break;
+                case "py":
+                    execPython(filePath);
+                    break;
+                default:
+                    System.out.println("Unknown file type");
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error: Invalid file name");
+        }
+    }
+
+    /*
+    ======================================================
+    ===[Main Helper Functions to Run Java, C++, Python]===
+    ======================================================
+    */
+
+    private static void execJava(String filePath) {
+        boolean compiled = compileJavaFile(filePath);
+
+        if (compiled)
+            executeCompiledJavaProgram(filePath);
+        else
+            System.out.println("Error: unable to execute Java file.");
+    }
+
+    private static void execCpp(String filePath) {
+        try {
+            ProcessBuilder compileProcess = new ProcessBuilder("g++", filePath, "-o", "output");
+            Process compile = compileProcess.start();
+            int compileExitCode = compile.waitFor();
+
+            if (compileExitCode == 0) {
+                ProcessBuilder executeProcess = new ProcessBuilder("./output");
+                Process execute = executeProcess.start();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(execute.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                reader.close();
+            } else {
+                System.out.println("C++ program compilation failed.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error: unable to execute C++ file.");
+        }
+    }
+
+    private static void execPython(String filePath) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("python", filePath);
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0)
+                System.out.println("Python script execution failed.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error: unable to execute Python file.");
+        }
+    }
+
+    /*
+    ======================================================
+    ======================================================
+    */
+
+    private static boolean compileJavaFile(String filePath) {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        int compilationResult = compiler.run(null, null, null, filePath);
+
+        return compilationResult == 0;
+    }
+
+    private static void executeCompiledJavaProgram(String filePath) {
+        try {
+            String className = getClassNameFromFilePath(filePath);
+            File classFileDirectory = new File(filePath);
+
+            URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{classFileDirectory.toURI().toURL()});
+            Class<?> clazz = Class.forName(className, true, classLoader);
+
+            Method mainMethod = clazz.getMethod("main", String[].class);
+
+            String[] params = {};
+            mainMethod.invoke(null, (Object) params);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error: unable to execute compiled Java class file.");
+        }
+    }
+
+    private static String getClassNameFromFilePath(String filePath) {
+        File file = new File(filePath);
+        String fileName = file.getName();
+
+        return fileName.substring(0, fileName.lastIndexOf('.'));
+    }
+
+    private static String getFileType(String filePath) {
+        int dotIndex = filePath.lastIndexOf('.');
+
+        if (dotIndex != -1 && dotIndex != filePath.length() - 1)
+            return filePath.substring(dotIndex + 1);
+
+        return "";
+    }
+
     public static void main(String[] args) {
 //        runListStatement();
 //        runPwdStatement();
@@ -186,7 +323,6 @@ public class Api {
 //        openStatement("test.txt");
 //        showPhraseStatement("hello", "test.txt");
 //        replaceStatement("test.txt", "hello", "hola");
+//        execStatement("EXECTestFiles/HelloWorld.java");
     }
 }
-
-
---
+++ EXECTestFiles/HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "C++ | hello world" << std::endl;
+    return 0;
+}
+
